91364233fb0a37ecd0f92883a06d60331117b4bf
Arnab S <arnabsengupta104@gmail.com>
2024-09-04 23:34:12 +0530

    API Fix

--- client/app/api/signup/route.ts
+++ client/app/api/signup/route.ts
@@ -17,46 +17,46 @@ interface UserRequestBody {
   confirmPassword: string;
 }
 
-export default async (req: NextRequest) => {
-  if (req.method === 'POST') {
-    const { email, password, username, confirmPassword }: UserRequestBody = await req.json();
-    
-    if (!email || !password || !username || !confirmPassword) {
-      return NextResponse.json({ message: 'Please fill all fields' }, { status: 400 });
-    }
-    
-    if (password !== confirmPassword) {
-      return NextResponse.json({ message: 'Passwords do not match' }, { status: 400 });
-    }
-
-    try {
-      await connectToDatabase();
-      
-      const User = mongoose.model('User', UserSchema);
-      const user = await User.findOne({ email });
-
-      if (user) {
-        return NextResponse.json({ message: 'User already exists' }, { status: 400 });
-      }
-
-      const hashedPassword = await hash(password, 12);
-      const newUser = new User({
-        email,
-        password: hashedPassword,
-        username
-      });
-      
-      await newUser.save();
-      
-      // Use `secretKey` with type assertion for JWT sign
-      const token = jwt.sign({ userId: newUser._id }, secretKey);
-
-      return NextResponse.json({ token, userId: newUser._id }, { status: 201 });
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    }
+async function posthandler (req: NextRequest) {
+  const { email, password, username, confirmPassword }: UserRequestBody = await req.json();
+  
+  if (!email || !password || !username || !confirmPassword) {
+    return NextResponse.json({ message: 'Please fill all fields' }, { status: 400 });
   }
   
-  return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
+  if (password !== confirmPassword) {
+    return NextResponse.json({ message: 'Passwords do not match' }, { status: 400 });
+  }
+
+  try {
+    await connectToDatabase();
+    
+    const User = mongoose.model('User', UserSchema);
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json({ message: 'User already exists' }, { status: 400 });
+    }
+
+    const hashedPassword = await hash(password, 12);
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      username
+    });
+    
+    await newUser.save();
+    
+    // Use `secretKey` with type assertion for JWT sign
+    const token = jwt.sign({ userId: newUser._id }, secretKey);
+
+    return NextResponse.json({ token, userId: newUser._id }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
 };
+
+export {
+  posthandler as POST
+}
